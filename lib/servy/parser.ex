a8655daf54924136e94433ecf3013742c7b05cba
Tommaso Amici <me@tommasoamici.com>
2023-09-09 23:42:09 +0200

    feat: parse headers

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -4,18 +4,26 @@ defmodule Servy.Parser do
   def parse(request) do
     [top, body] = String.split(request, "\n\n")
 
-    [request_line | _headers] = String.split(top, "\n")
+    [request_line | headers_lines] = String.split(top, "\n")
 
     [method, path, _] = String.split(request_line, " ")
 
     %Conv{
       method: method,
       path: path,
-      params: parse_params(body)
+      params: parse_params(body),
+      headers: parse_headers(%{}, headers_lines)
     }
   end
 
   def parse_params(body) do
     body |> String.trim() |> URI.decode_query()
+  end
+
+  def parse_headers(headers, []), do: headers
+
+  def parse_headers(headers, [head | tail]) do
+    [key, value] = String.split(head, ": ")
+    parse_headers(Map.put(headers, key, value), tail)
   end
 end
