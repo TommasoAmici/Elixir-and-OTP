b664a45b09efd3a28c8749f6dcb62992c9aba3d3
Tommaso Amici <me@tommasoamici.com>
2023-09-09 23:48:27 +0200

    fix: only parse content-type application/x-www-form-urlencoded

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -8,22 +8,26 @@ defmodule Servy.Parser do
 
     [method, path, _] = String.split(request_line, " ")
 
+    headers = parse_headers(%{}, headers_lines)
+
     %Conv{
       method: method,
       path: path,
-      params: parse_params(body),
-      headers: parse_headers(%{}, headers_lines)
+      headers: headers,
+      params: parse_params(headers["content-type"], body)
     }
   end
 
-  def parse_params(body) do
+  def parse_params("application/x-www-form-urlencoded", body) do
     body |> String.trim() |> URI.decode_query()
   end
+
+  def parse_params(_, _), do: %{}
 
   def parse_headers(headers, []), do: headers
 
   def parse_headers(headers, [head | tail]) do
     [key, value] = String.split(head, ": ")
-    parse_headers(Map.put(headers, key, value), tail)
+    parse_headers(Map.put(headers, key |> String.downcase(), value), tail)
   end
 end
