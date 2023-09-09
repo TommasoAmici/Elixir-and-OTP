01d43aed7aee899de937ea707b71905d7b4015cc
Tommaso Amici <me@tommasoamici.com>
2023-09-10 00:38:17 +0200

    feat: added html eex templates

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -3,24 +3,24 @@ defmodule Servy.BearController do
   alias Servy.Conv
   alias Servy.Wildthings
 
-  defp render_bear(bear) do
-    "<li><a href='/bears/#{bear.id}'>#{bear.name}</a></li>"
+  @templates_path Path.expand("../../templates", __DIR__)
+
+  defp render(conv, template, bindings) do
+    content = @templates_path |> Path.join(template) |> EEx.eval_file(bindings)
+    %{conv | status: 200, resp_body: content}
   end
 
   def index(%Conv{} = conv) do
     bears =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_by_name/2)
-      |> Enum.map(&render_bear/1)
-      |> Enum.join()
 
-    %{conv | status: 200, resp_body: "<ul>#{bears}</ul>"}
+    render(conv, "index.html.eex", bears: bears)
   end
 
   def show(%Conv{} = conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{conv | status: 200, resp_body: "<h1>#{bear.name}</h1>"}
+    render(conv, "show.html.eex", bear: bear)
   end
 
   def create(%Conv{} = conv, %{"name" => name, "type" => type}) do
