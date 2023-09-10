8beaf3fbafca02d923189ccb2da2d5e014248d34
Tommaso Amici <me@tommasoamici.com>
2023-09-10 12:40:15 +0200

    feat: added cast and generic handler

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -10,15 +10,36 @@ defmodule Servy.PledgeServer do
 
   def listen_loop(state) do
     receive do
-      {sender, message} when is_pid(sender) ->
+      {:call, sender, message} when is_pid(sender) ->
         {response, new_state} = handle_call(message, state)
         send(sender, {:response, response})
+        listen_loop(new_state)
+
+      {:cast, message} ->
+        new_state = handle_cast(message, state)
         listen_loop(new_state)
 
       unexpected ->
         IO.puts("Unpexected message #{inspect(unexpected)}")
         listen_loop(state)
     end
+  end
+
+  def call(pid, message) do
+    send(pid, {:call, self(), message})
+
+    receive do
+      {:response, response} ->
+        response
+    end
+  end
+
+  def cast(pid, message) do
+    send(pid, {:cast, message})
+  end
+
+  def handle_cast(:clear, _state) do
+    []
   end
 
   def handle_call(:total_pledged, state) do
@@ -40,15 +61,6 @@ defmodule Servy.PledgeServer do
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
 
-  def call(pid, message) do
-    send(pid, {self(), message})
-
-    receive do
-      {:response, response} ->
-        response
-    end
-  end
-
   def create_pledge(name, amount) do
     call(@name, {:create_pledge, name, amount})
   end
@@ -59,5 +71,9 @@ defmodule Servy.PledgeServer do
 
   def total_pledged() do
     call(@name, :total_pledged)
+  end
+
+  def clear() do
+    cast(@name, :clear)
   end
 end
--- start.exs
+++ start.exs
@@ -1,12 +1,16 @@
-Servy.PledgeServer.start()
+pid = Servy.PledgeServer.start()
 
 Servy.PledgeServer.create_pledge( "John", 100) |> IO.inspect()
 Servy.PledgeServer.create_pledge( "Al", 200) |> IO.inspect()
 Servy.PledgeServer.create_pledge( "Jack", 50) |> IO.inspect()
 Servy.PledgeServer.create_pledge( "Daisy", 600) |> IO.inspect()
+
+Servy.PledgeServer.clear() |> IO.inspect()
 Servy.PledgeServer.create_pledge( "Helena", 100) |> IO.inspect()
 
 Servy.PledgeServer.recent_pledges() |> IO.inspect()
 Servy.PledgeServer.total_pledged() |> IO.inspect()
+
+send(pid, {:stop, "hammertime"})
 
 Servy.HttpServer.start(4000)
