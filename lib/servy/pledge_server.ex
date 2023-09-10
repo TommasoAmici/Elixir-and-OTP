951d0b18c6471b6a50cf360de7e8f1c509b04d79
Tommaso Amici <me@tommasoamici.com>
2023-09-10 12:58:15 +0200

    feat: allow to change cache size

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -3,32 +3,40 @@ defmodule Servy.PledgeServer do
 
   use GenServer
 
+  defmodule State do
+    defstruct cache_size: 3, pledges: []
+  end
+
   def init(init_arg) do
     {:ok, init_arg}
   end
 
   def start() do
     IO.puts("Starting pledge server...")
-    GenServer.start(__MODULE__, [], name: @name)
+    GenServer.start(__MODULE__, %State{}, name: @name)
   end
 
-  def handle_cast(:clear, _state) do
-    {:noreply, []}
+  def handle_cast(:clear, state) do
+    {:noreply, %{state | pledges: []}}
+  end
+
+  def handle_cast({:set_cache_size, size}, state) do
+    {:noreply, %{state | cache_size: size}}
   end
 
   def handle_call(:total_pledged, _from, state) do
-    total = state |> Enum.map(&elem(&1, 1)) |> Enum.sum()
+    total = state.pledges |> Enum.map(&elem(&1, 1)) |> Enum.sum()
     {:reply, total, state}
   end
 
   def handle_call(:recent_pledges, _from, state) do
-    {:reply, state, state}
+    {:reply, state.pledges, state}
   end
 
   def handle_call({:create_pledge, name, amount}, _from, state) do
     {:ok, id} = send_pledge_to_service(name, amount)
-    new_state = [{name, amount} | state |> Enum.take(2)]
-    {:reply, id, new_state}
+    cached_pledges = [{name, amount} | state.pledges |> Enum.take(state.cache_size - 1)]
+    {:reply, id, %{state | pledges: cached_pledges}}
   end
 
   defp send_pledge_to_service(_name, _amount) do
@@ -49,5 +57,9 @@ defmodule Servy.PledgeServer do
 
   def clear() do
     GenServer.cast(@name, :clear)
+  end
+
+  def set_cache_size(size) do
+    GenServer.cast(@name, {:set_cache_size, size})
   end
 end
